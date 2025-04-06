e0f772d714c68fcc988fd1127a99efa322d232c9
cokefryburger <122137054+cokefryburger@users.noreply.github.com>
2025-04-05 23:14:51 -0400

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,23 @@
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
   title: "retro3d",
-  description: "retro3d",
+  description: "retro3d"
 };
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body className="font-mono antialiased">{children}</body>
+      <body className="font-mono antialiased">
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
